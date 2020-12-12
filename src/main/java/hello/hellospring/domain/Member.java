1f3b1006e7de673bc5279575158e8492fd3935f4
hanbee <sonhanbi1002@gmail.com>
2020-12-12 13:45:45 +0900

    jpa 코드 추가

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,17 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // 회원 ID (시스템이 지정하는 ID)
+
     private String name; // 회원 이름
 
     public Long getId() {
